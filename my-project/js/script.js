95fcf77fdb2266014762f0ed8e5b682169d8da72
dexter <dexterlopezumali@gmail.com>
2025-06-26 13:46:05 +0800

    update script

--- my-project/js/script.js
+++ my-project/js/script.js
@@ -42,6 +42,16 @@ if (form) {
   });
 }
 
+//Preloader
+window.addEventListener("load", function() {
+  const preloader = document.getElementById('preloader');
+  preloader.style.opacity = '0';
+  preloader.style.transition = 'opacity 0.5s ease';
+  setTimeout(() => {
+    preloader.style.display = 'none';
+  }, 500);
+});
+
 // Get the button
 const backToTopBtn = document.getElementById("backToTopBtn");
 
