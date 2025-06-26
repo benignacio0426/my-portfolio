8ac6d17694fb7b90175d4ff20486b71921794afe
dexter <dexterlopezumali@gmail.com>
2025-06-26 12:38:01 +0800

    update script

--- my-project/js/script.js
+++ my-project/js/script.js
@@ -21,3 +21,23 @@ AOS.init({
   duration: 1000, // Animation speed in ms
   once: true // Run animations only per element
 });
+
+//Toast Notification After Form Submit
+const form = document.querySelector('form');
+if (form) {
+  const toast = document.elementFromPoint('div');
+  toast.className =
+    'fixed bottom-8 right-8 bg-green-600 text-white px-4 py-2 rounded-lg shadow-lg hidden z-50';
+  toast.innerText = '✅ Message Sent Successfully!';
+  document.body.appendChild(toast);
+
+
+  form.addEventListener('submit', (e) =>{
+    setTimeout(() => {
+      toast.classList.remove('hidden');
+      setTimeout(() => {
+        toast.classList.add('hidden');
+      }, 3000);
+    }, 500);
+  });
+}
