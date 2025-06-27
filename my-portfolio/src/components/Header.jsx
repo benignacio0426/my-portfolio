48de4e533eff88ec420c28af335d3511a0ab039a
dexter <dexterlopezumali@gmail.com>
2025-06-27 14:49:38 +0800

    Header update

---
+++ my-portfolio/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Ben Ignacio</h1>
+      <nav>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
